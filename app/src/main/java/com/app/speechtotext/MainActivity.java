1fae169cf67d30d626be9feb30fc9393bbc3f837
wajdi <wajdi.chaouche@gmail.com>
2020-12-21 17:19:06 +0100

    update headset

--- app/src/main/java/com/app/speechtotext/MainActivity.java
+++ app/src/main/java/com/app/speechtotext/MainActivity.java
@@ -7,7 +7,9 @@ import androidx.core.content.ContextCompat;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothHeadset;
+import android.bluetooth.BluetoothManager;
 import android.bluetooth.BluetoothProfile;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
@@ -16,6 +18,7 @@ import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -28,7 +31,7 @@ import static android.Manifest.permission.RECORD_AUDIO;
 public class MainActivity extends AppCompatActivity {
 
     TextView tvText;
-    Button btnVoice;
+    Button btnVoice, btnHeadset, btnReset;
 
     SpeechRecognizer speechRecognizer;
 
@@ -38,18 +41,27 @@ public class MainActivity extends AppCompatActivity {
     BluetoothHeadset btHeadset;
     Set<BluetoothDevice> pairedDevices;
 
+    String resultMessage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btAdapter = BluetoothAdapter.getDefaultAdapter();
+        resultMessage = "";
+
+        btAdapter = checkBLESupportForAdapter(this);
+        if (btAdapter == null) {
+            Log.e("bluetoothAdapter","null");
+            btAdapter = BluetoothAdapter.getDefaultAdapter();
+        }
 
         pairedDevices = btAdapter.getBondedDevices();
 
         BluetoothProfile.ServiceListener mProfileListener = new BluetoothProfile.ServiceListener() {
             public void onServiceConnected(int profile, BluetoothProfile proxy) {
                 if (profile == BluetoothProfile.HEADSET) {
+                    Log.e("headset","profiler");
                     btHeadset = (BluetoothHeadset) proxy;
                 }
             }
@@ -62,14 +74,7 @@ public class MainActivity extends AppCompatActivity {
         };
         btAdapter.getProfileProxy(MainActivity.this, mProfileListener, BluetoothProfile.HEADSET);
 
-        if (btAdapter.isEnabled()) {
-            for (BluetoothDevice tryDevice : pairedDevices) {
-                //This loop tries to start VoiceRecognition mode on every paired device until it finds one that works(which will be the currently in use bluetooth headset)
-                if (btHeadset.startVoiceRecognition(tryDevice)) {
-                    break;
-                }
-            }
-        }
+
 
         isListening = false;
 
@@ -77,6 +82,36 @@ public class MainActivity extends AppCompatActivity {
 
         tvText = findViewById(R.id.tv_text);
         btnVoice = findViewById(R.id.btn_voice);
+        btnHeadset = findViewById(R.id.btn_headset);
+        btnReset = findViewById(R.id.btn_reset);
+
+        btnHeadset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (btAdapter.isEnabled()) {
+                    Log.e("bAdapter", "enabled");
+                    for (BluetoothDevice tryDevice : pairedDevices) {
+                        Log.e("device", tryDevice.getName());
+                        //This loop tries to start VoiceRecognition mode on every paired device until it finds one that works(which will be the currently in use bluetooth headset)
+                        if (btHeadset != null && btHeadset.startVoiceRecognition(tryDevice)) {
+                            Log.e("headset", tryDevice.getName());
+                            btnHeadset.setText("headset:"+tryDevice.getName());
+                            break;
+                        }
+                    }
+                }else {
+                    Log.e("bAadapter","disabled");
+                }
+            }
+        });
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resultMessage = "";
+                tvText.setText("");
+            }
+        });
 
         final Intent speechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
@@ -106,6 +141,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onEndOfSpeech() {
                 Log.e("onEndOfSpeech", "yes");
+                isListening = false;
+                btnVoice.setText("start");
             }
 
             @Override
@@ -116,12 +153,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResults(Bundle results) {
                 ArrayList<String> data = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-                tvText.setText(data.get(0));
+                if (resultMessage.isEmpty()) {
+                    tvText.setText(data.get(0));
+                }
             }
 
             @Override
             public void onPartialResults(Bundle partialResults) {
                 Log.e("onPartialResults", "yes");
+                ArrayList<String> data = partialResults.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                resultMessage = resultMessage+data.get(0);
+                tvText.setText(resultMessage);
             }
 
             @Override
@@ -144,6 +186,19 @@ public class MainActivity extends AppCompatActivity {
             checkPermission();
         }
 
+    }
+
+    public static BluetoothAdapter checkBLESupportForAdapter(Context context) {
+        if (!context.getPackageManager().hasSystemFeature("android.hardware.bluetooth")) {
+            return null;
+        } else {
+            BluetoothManager bluetoothManager = (BluetoothManager)context.getSystemService(Context.BLUETOOTH_SERVICE);
+            if (bluetoothManager == null) {
+                return null;
+            }else {
+                return bluetoothManager.getAdapter();
+            }
+        }
     }
 
     private void checkPermission() {
